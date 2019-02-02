32af8040c54a908f8ad96f62f82ce24424125c5c
tac-yacht <tac-yacht@users.noreply.github.com>
2019-02-02 10:15:05 +0900

    add minimum webApp

---
+++ src/main/java/com/example/Controller.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+	@GetMapping("/")
+	public String root() {
+		return "hello world";
+	}
+
+}
